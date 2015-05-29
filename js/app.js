29925cb008d42056688d6862f88942cdfd21b295
aespinozagit <aespinoza123@gmail.com>
2015-05-29 15:48:31 -0500

    Added animaiton to hadouken

--- js/app.js
+++ js/app.js
@@ -8,9 +8,23 @@ $(document).ready(function() {
     $('.ryu-ready').hide();
   })
   .mousedown(function() {
-  	$('.ryu-still').hide();
+    //Play hadouken sound
+    //Show hadouken and animate it to the right of the screen
   	$('.ryu-ready').hide();
   	$('.ryu-throwing').show();
+    $('.hadouken').finish().show().animate(
+      {'left': '1020px'}, 500,
+      function() {
+        $(this).hide();
+        $(this).css('left', '520px');
+      });
+
+  })
+  .mouseup(function() {
+    //Ryu goes back to his ready position
+    $('.ryu-ready').show();
+    $('.ryu-throwing').hide();
+
   })
   ;
  
