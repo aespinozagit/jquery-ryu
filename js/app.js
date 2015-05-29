254380e3882e05c7f80790c9119d575ead4e251f
Alex Espinoza <aespinoza123@gmail.com>
2015-05-28 22:28:07 -0500

    Added jQuery show and hide

---
+++ js/app.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+  $('.ryu').mouseenter(function() {
+    $('.ryu-still').hide();
+    $('.ryu-ready').show();
+  })
+  .mouseleave(function() {
+    $('.ryu-still').show();
+    $('.ryu-ready').hide();
+  })
+  .mousedown(function() {
+  	$('.ryu-still').hide();
+  	$('.ryu-ready').hide();
+  	$('.ryu-throwing').show();
+  })
+  ;
+ 
+
+});
