2c1e442c29eba763bb0dd0f81b46083a30cc1f40
Alex Espinoza <aespinoza123@gmail.com>
2015-05-29 22:39:32 -0500

    Added ryu-cool

--- js/app.js
+++ js/app.js
@@ -3,12 +3,15 @@ $(document).ready(function() {
     $('.ryu-still').hide();
     $('.ryu-ready').show();
   })
+
   .mouseleave(function() {
     $('.ryu-still').show();
     $('.ryu-ready').hide();
   })
+
   .mousedown(function() {
-    //Play hadouken sound
+  	//Play hadouken sound
+  	playHadouken();
     //Show hadouken and animate it to the right of the screen
   	$('.ryu-ready').hide();
   	$('.ryu-throwing').show();
@@ -18,15 +21,33 @@ $(document).ready(function() {
         $(this).hide();
         $(this).css('left', '520px');
       });
-
   })
+
   .mouseup(function() {
     //Ryu goes back to his ready position
     $('.ryu-ready').show();
     $('.ryu-throwing').hide();
+  });
 
-  })
-  ;
- 
+
+    $(document).keydown(function(e) {
+    if (e.keyCode == 88) {
+      $('.ryu-ready').hide();
+      $('.ryu-still').hide();
+      $('.ryu-cool').show();
+    }   
+  }).keyup(function(e) {
+    if (e.keyCode == 88) {
+      $('.ryu-cool').hide();
+      $('.ryu-still').show();
+    }
+  });
+             
 
 });
+
+   function playHadouken() {
+    	$('#hadouken-sound')[0].volume = 0.5;
+    	$('#hadouken-sound')[0].load();
+    	$('#hadouken-sound')[0].play();
+    }
