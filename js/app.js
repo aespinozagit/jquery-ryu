b81dc33e990503be0adcaf382576c15aff0a9164
Alex Espinoza <aespinoza123@gmail.com>
2015-05-31 20:08:02 -0500

    Removed border

--- js/app.js
+++ js/app.js
@@ -52,10 +52,12 @@ function playHadouken() {
 	$('#hadouken-sound')[0].volume = 0.5;
 	$('#hadouken-sound')[0].load();
 	$('#hadouken-sound')[0].play();
+	$('#intro-song')[0].pause();
 }
 
 function playCool() {    	
     	$('#cool-sound')[0].play();
+    	$('#intro-song')[0].pause();
     }
 
 function runIntro() {
